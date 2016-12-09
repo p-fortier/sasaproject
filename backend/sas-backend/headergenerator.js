49320dba1c5eaf2b1575909a20ca1b9c43d14a87
llalanne <lucielalanne@hotmail.fr>
2016-12-09 09:40:42 +0100

    peuplement de la base de donnees et affichage dans /allheaders

--- backend/sas-backend/headergenerator.js
+++ backend/sas-backend/headergenerator.js
@@ -2,7 +2,8 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 const crypto = require('crypto');
 
-MongoClient.connect("mongodb://localhost/sasproject", function(error, db) {
+function getAllHeaders(req, res, next){
+  MongoClient.connect("mongodb://localhost/sasproject", function(error, db) {
     if (error) {
       console.log('Erreur : ', error);
     }
@@ -12,7 +13,7 @@ MongoClient.connect("mongodb://localhost/sasproject", function(error, db) {
 
     var collection = db.collection('headers');
 
-    var header1 = {name: 'patrik', age: 22};
+    /*var header1 = {name: 'patrik', age: 22};
     var header2 = {name: 'lulu', age: 22};
 
     collection.insert([header1, header2], function (err, res) {
@@ -21,14 +22,16 @@ MongoClient.connect("mongodb://localhost/sasproject", function(error, db) {
       } else {
         console.log('headers insérés dans la base')
       }
-      db.close();
-    });
+      //db.close();
+    });*/
 
     collection.find().toArray(function (err, result){
       if (err) {
         console.log('Erreur : ', err);
       } else if (result.length) {
-        console.log('Found : ', result);
+        console.log('Headers trouvés!');
+        res.status(200)
+          .json(result);
       } else{
         console.log('Pas de document trouvé !');
       }
@@ -38,6 +41,7 @@ MongoClient.connect("mongodb://localhost/sasproject", function(error, db) {
   }
 
 });
+}
 
 
 function test(req, res, next) {
@@ -61,7 +65,9 @@ function getHeaders(req, res, next) {
     .json(jsonResponse);
 }
 
+
 module.exports = {
   test: test,
-  getHeaders: getHeaders
+  getHeaders: getHeaders,
+  getAllHeaders: getAllHeaders
 }
